231d0edb7efc706a72e08e865fda074cb861ee86
archismicro <archismicro@gmail.com>
2018-09-20 15:53:33 -0500

    Second Version

---
+++ TestAddressbook_TC/src/main/java/TestAddressbook_TC/TestAddressbook_TC/App.java
@@ -0,0 +1,13 @@
+package TestAddressbook_TC.TestAddressbook_TC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
