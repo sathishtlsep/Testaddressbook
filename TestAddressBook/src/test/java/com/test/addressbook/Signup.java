836022697cac273bf8d3fc7435621f0af620fbad
simmisharma <sshelza01@gmail.com>
2018-09-22 09:51:24 -0500

    Signup validate

---
+++ TestAddressBook/src/test/java/com/test/addressbook/Signup.java
@@ -0,0 +1,51 @@
+package com.test.addressbook;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Signup {
+WebDriver driver;
+	
+	@BeforeClass
+	public void doBefore() {
+		
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\sharm\\Documents\\Shelza\\chromedriver.exe");
+	driver = new ChromeDriver();
+	//driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.get("http://a.testaddressbook.com/sign_up");
+	
+
+}
+	@Test
+	public void validatesignup() {
+	
+	
+				
+		driver.findElement(By.xpath("//*[@id='user_email']")).sendKeys("she3za@g0a53.com");
+		driver.findElement(By.xpath("//*[@id=\"user_password\"]")).sendKeys("sharma");
+		driver.findElement(By.xpath("//*[@id=\"new_user\"]/div[3]/input")).click();
+		
+	}
+	@AfterClass
+	public void aftersignup() {
+		driver.quit();
+				
+	
+	
+	
+	
+	
+	
+		
+		
+	}
+}
